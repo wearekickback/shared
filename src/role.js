f89ebc26e4b42804c6959c61c20f418fab096f60
Ramesh Nair <ram@hiddentao.com>
2019-01-05 22:06:49 +0000

    1.9.2 - bugfix

--- src/role.js
+++ src/role.js
@@ -7,6 +7,14 @@ export const ROLE = {
 
 export const userHasRole = (user, role) => (user.roles || []).includes(role)
 
-export const extractUsersWithGivenEventRole = (event, r) => event.roles.filter(({ role }) => role === r).map(({ user }) => user)
+export const extractUsersWithGivenEventRole = (event, r) => (
+  event.roles
+    .filter(({ role }) => role === r)
+    .map(({ user }) => user)
+)
 
-export const userHasEventRole = (addressOrId, event, r) => !!event.roles.find(({ role, user }) => role === r && (user.id === addressOrId || addressesMatch(user.address, addressOrId)))
+export const userHasEventRole = (addressOrId, event, r) => !!(
+  event.roles.find(({ role, user }) => (
+    role === r && (user.id === addressOrId || addressesMatch(user.address, addressOrId))
+  ))
+)
