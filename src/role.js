ee5e7043bdeef61a2763f26507192aa6d4729248
Ramesh Nair <ram@hiddentao.com>
2019-01-05 21:52:17 +0000

    1.9.0 - roles

--- src/role.js
+++ src/role.js
@@ -2,3 +2,7 @@ export const ROLE = {
   SUPER_ADMIN: 'SUPER_ADMIN',
   EVENT_ADMIN: 'EVENT_ADMIN',
 }
+
+export const userHasRole = (user, role) => (user.roles || []).includes(role)
+
+export const extractUsersWithGivenEventRole = (event, r) => event.roles.filter(({ role }) => role === r).map(({ user }) => user)
--- src/role.test.js
+++ src/role.test.js
@@ -1,5 +1,7 @@
 import {
-  ROLE
+  ROLE,
+  userHasRole,
+  extractUsersWithGivenEventRole,
 } from './'
 
 describe('role', () => {
@@ -8,5 +10,44 @@ describe('role', () => {
       SUPER_ADMIN: 'SUPER_ADMIN',
       EVENT_ADMIN: 'EVENT_ADMIN',
     })
+  })
+})
+
+describe('userHasRole', () => {
+  it('works', () => {
+    expect(userHasRole({ roles: [] }, 'test')).toEqual(false)
+    expect(userHasRole({ roles: [ 'test' ] }, 'test')).toEqual(true)
+    expect(userHasRole({ roles: [ 'test0', 'test' ] }, 'test')).toEqual(true)
+    expect(userHasRole({}, 'test')).toEqual(false)
+  })
+})
+
+describe('extractUsersWithGivenEventRole', () => {
+  it('works', () => {
+    const event = {
+      roles: [
+        {
+          role: ROLE.SUPER_ADMIN,
+          user: 1,
+        },
+        {
+          role: 'test',
+          user: 2,
+        },
+        {
+          role: ROLE.EVENT_ADMIN,
+          user: 3,
+        },
+        {
+          role: ROLE.SUPER_ADMIN,
+          user: 4,
+        },
+      ]
+    }
+
+    expect(extractUsersWithGivenEventRole(event, 'unknown')).toEqual([])
+    expect(extractUsersWithGivenEventRole(event, 'test')).toEqual([ 2 ])
+    expect(extractUsersWithGivenEventRole(event, ROLE.SUPER_ADMIN)).toEqual([ 1, 4 ])
+    expect(extractUsersWithGivenEventRole(event, ROLE.EVENT_ADMIN)).toEqual([ 3 ])
   })
 })
